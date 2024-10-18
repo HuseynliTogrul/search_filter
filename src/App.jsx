932afb5c215c0f9435ac7748754aea8f956eda40
HuseynliTogrul <toghrulih@code.edu.az>
2024-10-18 15:23:43 +0400

    add not found

--- src/App.jsx
+++ src/App.jsx
@@ -44,14 +44,22 @@ function App() {
           {
             contact?.filter((item) =>
               item.first_name.toLowerCase().includes(search.toLowerCase())
-            ).map((item) => (
-              <tr key={item.id}>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <th>{item.phone}</th>
+            ).length > 0 ? (
+              contact?.filter((item) =>
+                item.first_name.toLowerCase().includes(search.toLowerCase())
+              ).map((item) => (
+                <tr key={item.id}>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <th>{item.phone}</th>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4"> Not Found</td>
               </tr>
-            ))
+            )
           }
         </tbody>
       </table>
